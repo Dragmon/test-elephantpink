c99459b9f5c84376f17cc8dcc25233f3dbff310a
Carlos Gerardo Perez Horta <carlos.perez@charliegperez.com>
2021-01-19 01:54:43 -0600

    chore(components):Creating the Header, IconBase and IconLock components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main>
+    <Header />
+  </main>
 </template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Header from "./components/Header.vue";
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+  components: { Header }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html {
+  font-size: 62.5%;
+}
+body {
+  margin: 0px;
+  box-sizing: border-box;
+}
+h1 {
+  font-size: 2.5rem;
+}
+h2 {
+  font-size: 2rem;
+}
+h3,
+h4,
+h5,
+h6 {
+  font-size: 1.8rem;
+}
+p {
+  font-size: 1.6rem;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+  <header>
+    <div class="login">
+      <icon-base width="12" height="12" icon-name="lock"
+        ><icon-lock />
+      </icon-base>
+      Login
+    </div>
+    <div>
+      <nav>
+        <ul>
+          <li><a href="#">HOME</a></li>
+          <li><a href="#">ABOUT</a></li>
+          <li><a href="#">Logo</a></li>
+          <li><a href="#">JEWELS</a></li>
+          <li><a href="#">CONTACT</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="search">
+      Search
+    </div>
+  </header>
+</template>
+
+<script>
+import IconBase from "./IconBase";
+import IconLock from "./icons/IconLock";
+export default {
+  name: "Header",
+  components: {
+    IconBase,
+    IconLock
+  }
+};
+</script>
+
+<style scoped>
+header {
+  height: 100px;
+  background-color: cadetblue;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 10%;
+}
+</style>
